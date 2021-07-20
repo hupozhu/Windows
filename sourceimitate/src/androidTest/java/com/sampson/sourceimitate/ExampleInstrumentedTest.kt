fe0ee1131f2a72c2e57fb609cf8f35d220d5804b
chengyang <349849907@qq.com>
2021-07-20 10:52:31 +0800

    add:单元测试

--- sourceimitate/src/androidTest/java/com/sampson/sourceimitate/ExampleInstrumentedTest.kt
+++ sourceimitate/src/androidTest/java/com/sampson/sourceimitate/ExampleInstrumentedTest.kt
@@ -2,11 +2,16 @@ package com.sampson.sourceimitate
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.sampson.sourceimitate.rxjava.*
+import com.sampson.sourceimitate.rxjava.Observable
+import com.sampson.sourceimitate.rxjava.Observer
+import io.reactivex.rxjava3.functions.Function
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import java.util.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,5 +25,28 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.sampson.sourceimitate.test", appContext.packageName)
+    }
+
+    @Test
+    fun testRxjavaDemo() {
+        Observable
+            .create(object : ObservableOnSubscribe<String> {
+                override fun subscribe(emitter: ObservableEmitter<String>) {
+                    println("1:=============" + Thread.currentThread().name)
+                    emitter.onNext("xxx")
+                }
+            })
+            .map(Function<String, String> {
+                println("2:=============" + Thread.currentThread().name)
+                it.toUpperCase(Locale.ROOT)
+            })
+            .subscribeOn(IoScheduler())
+            .observerOn(MainHandlerScheduler())
+            .subscribe(object : Observer<String> {
+                override fun onNext(t: String) {
+                    println("3:=============" + Thread.currentThread().name)
+                    assertEquals(t, "XXX")
+                }
+            })
     }
 }
